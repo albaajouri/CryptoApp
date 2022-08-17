bdfbc1cb3b590b03d883415694d3a94bdaee0c9a
albaajouri <mohamed.bajouri@hotmail.com>
2022-08-17 11:38:06 +0200

    adding the folder

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '20c2759552msh995aa92ee7bd9c1p137a1ejsn05eb69b530f3',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+}
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com/';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
